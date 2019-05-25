a9076178315db6cd09c201f65a6db7fee4a137bb
Administrator <18333510375@163.com>
2019-05-25 17:18:44 +0800

    第四天重入锁的图解

--- src/main/java/com/one/Main.java
+++ src/main/java/com/one/Main.java
@@ -1,12 +1,17 @@
 package com.one;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 public class Main {
     /**
      * 使用线程池优化
      * @param args
      */
     public static void main(String[] args) {
-
+        ReentrantReadWriteLock reentrantReadWriteLock=new ReentrantReadWriteLock();
+        Lock readLock = reentrantReadWriteLock.readLock();
+        Lock writeLock = reentrantReadWriteLock.writeLock();
         Handle.send();
     }
 
